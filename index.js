2882ecf046b56c2892d2f2f4edab3bccfcd793b3
Nelson Moreno <thegreyabove@gmail.com>
2024-12-03 18:01:26 -0500

    Looping Lab commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++)
+    console.log(`I'm ${age} years old. Happy Birthday to me!`)
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++)
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`)
+    return gifts
+}
+
+const thankMessages = []
+
+function writeCards(names, eventName) {
+    
+    for (let i = 0; i < names.length; i++)
+       thankMessages[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+
+    return thankMessages
+}
+
+
+wrapGifts(gifts)
+writeCards(["Charlie","Samip","Ali"], "birthday")
+console.log(thankMessages)
+
+function countDown(num) {
+    while(num > -1) {
+        console.log(num)
+        --num
+    }
+}
+
+let num = 10
+countDown(num)
